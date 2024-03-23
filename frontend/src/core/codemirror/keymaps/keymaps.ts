95f4a910ff8de05c81e0eda7be37606ab0a7ec47
Luca Mondada <luca@mondada.net>
2024-03-23 19:47:58 +0100

    Revert "Use options object as argument"
    This reverts commit 858078fd72c97858b70759979d965f9f5793def1.

--- frontend/src/core/codemirror/keymaps/keymaps.ts
+++ frontend/src/core/codemirror/keymaps/keymaps.ts
@@ -14,7 +14,7 @@ export function keymapBundle(
   callbacks: {
     focusUp: () => void;
     focusDown: () => void;
-    deleteCell: (options?: { deleteIfFirst: boolean }) => void;
+    deleteCell: (deleteIfFirst?: boolean) => void;
   },
 ): Extension[] {
   switch (config.preset) {
@@ -38,7 +38,7 @@ export function keymapBundle(
             run: (cm) => {
               // Delete cell if it is empty and it is not the first cell
               if (cm.state.doc.toString() === "") {
-                callbacks.deleteCell({ deleteIfFirst: false });
+                callbacks.deleteCell(false);
                 return true;
               }
               return false;
